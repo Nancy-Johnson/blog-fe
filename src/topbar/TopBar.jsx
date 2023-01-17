04b88f63a776af8cc98971ad3b0c5f1bd9577261
Nancy Johnson <nan.milestones@gmail.com>
2023-01-17 22:03:20 +0530

    this is new commit on 17/01/23

--- src/topbar/TopBar.jsx
+++ src/topbar/TopBar.jsx
@@ -3,7 +3,25 @@ import "./topbar.css"
 export default function TopBar(){
     return (
         <div className="top">
-            topbar
+           <div className="topLeft">
+            <i class="fa-brands fa-square-facebook"></i>
+            <i class="fa-brands fa-square-twitter"></i>
+           <i class="fa-brands fa-square-pinterest"></i>
+           <i class="fa-brands fa-square-instagram"></i>
+            </div>
+           <div className="topCenter">
+            <ul ClassName="toplist">
+                <li ClassName="topListItem">HOME</li>
+                <li ClassName="topListItem">ABOUT</li>
+                <li ClassName="topListItem">CONTACT</li>
+                <li ClassName="topListItem">WRITE</li>
+                <li ClassName="topListItem">LOGOUT</li>
+            </ul>
+           
+           </div>
+           <div className="topRight">
+           
+           </div>
         </div>
     )
 }
