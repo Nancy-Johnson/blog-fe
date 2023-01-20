51d1ac2b406b24bb0a2f015282a4ffd9bd36f774
Nancy Johnson <nan.milestones@gmail.com>
2023-01-20 16:01:23 +0530

    new commit files on 20/01/23

--- src/topbar/TopBar.jsx
+++ src/topbar/TopBar.jsx
@@ -4,10 +4,10 @@ export default function TopBar(){
     return (
         <div className="top">
            <div className="topLeft">
-            <i class="fa-brands fa-square-facebook"></i>
-            <i class="fa-brands fa-square-twitter"></i>
-           <i class="fa-brands fa-square-pinterest"></i>
-           <i class="fa-brands fa-square-instagram"></i>
+            <i className="topIcon fa-brands fa-square-facebook"></i>
+            <i className="topIcon fa-brands fa-square-twitter"></i>
+           <i className="topIcon fa-brands fa-square-pinterest"></i>
+           <i className="topIcon fa-brands fa-square-instagram"></i>
             </div>
            <div className="topCenter">
             <ul ClassName="toplist">
@@ -20,6 +20,12 @@ export default function TopBar(){
            
            </div>
            <div className="topRight">
+            <img
+                ClassName="topImg"
+                src="C:\Users\NANCY JOHNSON\Downloads\marguerite-729510__340.jpg"
+                alt="not working "
+            />
+            <i className="topSearchIcon fa-solid fa-magnifying-glass"></i>
            
            </div>
         </div>
