c8d2ae8995df00145abe0a70c74f0994298f01c2
Nancy Johnson <nan.milestones@gmail.com>
2023-02-15 20:46:17 +0530

    15/02/23

--- src/App.js
+++ src/App.js
@@ -1,19 +1,25 @@
-/*import Home from "./pages/home/home";*/
+import Home from "./pages/home/home";
 import TopBar from "./components/topbar/TopBar";
-import Login from "./pages/login/Login";
+/*import Login from "./pages/login/Login";*/
+/*import Register from "./pages/register/Register";*/
 /*import Settings from "./pages/settings/Settings";*/
 /*import Single from "./pages/single/Single";*/
 /*import Write from "./pages/write/Write";*/
+import { BrowserRouter as Router, Switch, Route, Link}  from "react-router-dom";
 
 function App() {
   return (
-    <>
-     
+    
+     <Router>
      <TopBar/>
-     <Login/>
-   
+     <Switch>
+      <Route path="/">
+        <Home />
+      </Route>
+     </Switch>
+    </Router>
       
-    </>
+    
   );
 }
 
--- src/pages/login/Login.jsx
+++ src/pages/login/Login.jsx
@@ -3,11 +3,13 @@ import "./login.css"
 export default function Login() {
     return(
         <div className="login">
+          <span className="loginTitle">Login</span>
+
             <form className="loginForm">
                 <label>Email</label>
-                <input type="text" placeholder="Enter your email......" />
+                <input type="text" className="loginInput" placeholder="Enter your email......" />
                 <label>Password</label>
-                <input type="text" placeholder="Enter your password......" />
+                <input type="text" className="loginInput" placeholder="Enter your password......" />
                 <button className="loginButton">Login</button>
                 <button className="loginRegisterButton">Register</button>
             </form>
